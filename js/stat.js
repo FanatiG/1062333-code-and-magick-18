a935286f8166171cb9637b164c99254265b579bf
FanatiG <antz96@mail.ru>
2019-09-16 23:14:18 +0300

    fixed eslint problems

--- js/stat.js
+++ js/stat.js
@@ -2,22 +2,22 @@
 
 window.renderStatistics = function (ctx, names, times) {
   ctx.lineWidth = 5;
-  ctx.fillStyle = "rgba(0,0,0,0.7)";
+  ctx.fillStyle = 'rgba(0,0,0,0.7)';
   ctx.fillRect(110, 20, 430, 280);
-  ctx.fillStyle = "#FFF";
+  ctx.fillStyle = '#FFF';
   ctx.fillRect(100, 10, 420, 270);
-  ctx.font = "16px PT Mono";
-  ctx.fillStyle = "black";
-  ctx.textBaseline = "hanging";
-  var message = "Ура вы победили!\nСписок результатов:";
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = 'black';
+  ctx.textBaseline = 'hanging';
+  var message = 'Ура вы победили!\nСписок результатов:';
   message.split('\n').forEach(function (str, shift) {
     ctx.fillText(str, 120, 30 + 20 * shift);
   });
 
 
-  for (let i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     if (names[i] === 'Вы') {
-      ctx.fillStyle = "rgba(255, 0, 0, 1)";
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = setHSL();
     }
@@ -30,9 +30,9 @@ window.renderStatistics = function (ctx, names, times) {
   }
 
   function setHSL() {
-    let h = 233;
-    let s = Math.floor(Math.random() * 100) + '%';
-    let l = 30 + '%';
+    var h = 233;
+    var s = Math.floor(Math.random() * 100) + '%';
+    var l = 30 + '%';
     return 'hsl(' + h + ',' + s + ',' + l + ')';
   }
-}
+};
