bd05c621e70a2b9f84d2521a33ea99366e1d0f6a
FanatiG <antz96@mail.ru>
2019-09-24 19:23:15 +0300

    fixed issues (24.09)

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var FIRSTNAMES = [
+var FIRST_NAMES = [
   'Иван',
   'Хуан',
   'Себастьян',
@@ -11,7 +11,7 @@ var FIRSTNAMES = [
   'Люпита',
   'Вашингтон'
 ];
-var SECONDNAMES = [
+var LAST_NAMES = [
   'да Марья',
   'Верон',
   'Мирабелла',
@@ -21,7 +21,7 @@ var SECONDNAMES = [
   'Нионго',
   'Ирвинг'
 ];
-var COATCOLORS = [
+var COAT_COLORS = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
   'rgb(146, 100, 161)',
@@ -29,38 +29,46 @@ var COATCOLORS = [
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'
 ];
-var EYESCOLORS = [
+var EYES_COLORS = [
   'black',
   'red',
   'blue',
   'yellow',
   'green'
 ];
-var mageVariants = [];
-var MAGEQUANTITY = 4;
+var MAGE_QUANTITY = 4;
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
+function getRandomElementFromArray(arr) {
+  return arr[Math.floor(Math.random() * Math.floor(arr.length))];
+}
+
 function generateData() {
-  for (var i = 0; i < MAGEQUANTITY; i++) {
+  var mageVariants = [];
+  for (var i = 0; i < MAGE_QUANTITY; i++) {
     var obj = {
-      name: FIRSTNAMES[Math.floor(Math.random() * Math.floor(FIRSTNAMES.length))] + ' ' + SECONDNAMES[Math.floor(Math.random() * Math.floor(SECONDNAMES.length))],
-      coatColor: COATCOLORS[Math.floor(Math.random() * Math.floor(COATCOLORS.length))],
-      eyesColor: EYESCOLORS[Math.floor(Math.random() * Math.floor(EYESCOLORS.length))]
+      name: getRandomElementFromArray(FIRST_NAMES) + '  ' + getRandomElementFromArray(LAST_NAMES),
+      coatColor: getRandomElementFromArray(COAT_COLORS),
+      eyesColor: getRandomElementFromArray(EYES_COLORS)
     };
-    mageVariants[i] = obj;
+    mageVariants.push(obj);
   }
+  return mageVariants;
+}
+
+function renderWizard(wizardElement, i) {
+  document.querySelector('.setup-similar-list').appendChild(wizardElement);
+  wizardElement.querySelector('.setup-similar-label').textContent = generateData()[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = generateData()[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = generateData()[i].eyesColor;
 }
 
 function showData() {
-  generateData();
   document.getElementsByClassName('setup')[0].classList.remove('hidden');
   document.getElementsByClassName('setup-similar')[0].classList.remove('hidden');
-  for (var i = 0; i < mageVariants.length; i++) {
+  for (var i = 0; i < generateData().length; i++) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
-    document.querySelector('.setup-similar-list').appendChild(wizardElement);
-    wizardElement.querySelector('.setup-similar-label').innerHTML = mageVariants[i].name;
-    wizardElement.querySelector('.wizard-coat').style.fill = mageVariants[i].coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = mageVariants[i].eyesColor;
+    renderWizard(wizardElement, i);
   }
 }
 showData();
